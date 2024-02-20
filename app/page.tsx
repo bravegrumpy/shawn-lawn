455e7412af4ff3e81b2cdd8fc56826dee14a7f5a
Robin Zollner <robin.zollner@protonmail.com>
2024-02-19 22:24:14 -0500

    Finally got center and flex box to work

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,33 @@
-'use client';
 export default function Home(){
     return(
         <>
             <header>
-                <h1>ShawnLawn</h1>
-                <p>A Charleston landscaping business</p>
+                <div className="centered">
+                    <h1>ShawnLawn</h1>
+                    <p>A Charleston landscaping business</p>
+                </div>
             </header>
             <style jsx>{`
                 header {
-                    backgound-color: green;
+                    margin: auto;
+                    height: 500px;
+                    width: 1000px;
+                }
+                .centered {
+                    color: brown;
+                    border: 3px solid blueviolet;
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: center;
+                }
+                h1 {
+
                 }
                 p {
                     color: blue;
                     font-family: serif;
                     backgound-color: pink;
+                    text-align: center;
                 }
                 `}</style>
         </>
