fe7f018e494a10724bb099ea03b549fa03dd630e
Robin Zollner <code@bravegrumpy.com>
2024-02-23 17:08:12 -0500

    minor color changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }){
     return(
         <html lang="en-us">
-            <body>{children}</body>
+            <body className="bg-gradient-to-b from-emerald-200 dark:from-emerald-950 from-10% dark:from-10% via-lime-100 dark:via-lime-950 via-40% to-green-100 dark:to-green-950 to-80% bg-no-repeat">{children}</body>
         </html>
     );
 }
