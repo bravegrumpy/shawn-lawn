05b2af5c564b967f5c5d3f577cff81123ed05f8c
Robin Zollner <code@bravegrumpy.com>
2024-02-22 21:39:38 -0500

    more experimentation with styles. gave up plugin

--- app/components/shawn-lawn.tsx
+++ app/components/shawn-lawn.tsx
@@ -1,7 +1,7 @@
 export default function ShawnLawn(){
     return(
         <>
-        <h1 className="flex flex-row justify-center items-center gap-0 text-center basis-0 uppercase border-solid border-2 border-amber-300 text-6xl/10 py-6 tracking-normal font-black text-amber-900 bg-green-600 [-webkit-text-stroke-width:3px] [-webkit-text-stroke-color:blue]">
+        <h1 className="flex flex-row justify-center items-center gap-0 text-center basis-0 uppercase border-solid border-2 border-amber-300 text-6xl/10 py-6 tracking-normal font-black text-amber-900 bg-green-600 text-stroke">
             <div className="basis-0 grow-1 shring-1 first-letter:text-8xl/10 text-end pr-1">
                 Shawn
             </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   },
   plugins: [
     require('tailwindcss'),
-    require('autoprefixer')
+    require('autoprefixer'),
   ],
 };
 export default config;
