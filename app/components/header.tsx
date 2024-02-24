824cf9296f86dd37a286c243a0c0f61c78b832ca
Robin Zollner <code@bravegrumpy.com>
2024-02-23 21:21:16 -0500

    adding envelope icon for list

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,6 +5,10 @@ export default function Header() {
         <>
             <header className="container w-max-9/12 w-min-9/12 w-9/12 flex flex-row justify-start items-center mx-auto border-solid border-0 border-b-2 border-neutral-500 border-collapse bg-emerald-100 dark:bg-emerald-950 dark:text-limeade-100 sticky top-0">
                 <ShawnLawn />
+                {/*
+                    TODO: #35 Add nav links
+                    TODO: #34 Extract nav-links into separate component 
+                 */}
             </header>
         </>
     );
