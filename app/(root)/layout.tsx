601246e79bbe42f8d11fe6636a3a0200038b2e66
Robin Zollner <code@bravegrumpy.com>
2024-02-28 02:20:47 -0500

    trying to follow outdated Next-Auth tutorial.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,20 @@
 import "./globals.css";
 import { ubuntu  } from "@/fonts";
+import { Provider } from 'next-auth/client';
 
 export default function RootLayout({
     children,
+    session
 }: {
     children: React.ReactNode
 }){
     return(
+        <>
+        <Provider session={session}
         <html lang="en-us">
             <body className={`${ubuntu.className} bg-gradient-to-b from-emerald-200 dark:from-emerald-950 from-10% dark:from-10% via-lime-100 dark:via-lime-950 via-40% to-green-100 dark:to-green-950 to-80% bg-no-repeat`}>{children}</body>
         </html>
+        </Provider>
+        </>
     );
 }
