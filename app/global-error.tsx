a3888e056be125e6347d84427f4ab36f6e52f5ab
Robin Zollner <code@bravegrumpy.com>
2024-02-29 18:14:45 -0500

    adding a global-error.tsx

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,3 +1,9 @@
 'use client';
 
-export default function GlobalError()
+export default function GlobalError({
+    error,
+    reset,
+}:{
+    error: Error & { digest?: string },
+    reset: () => void
+}){}
