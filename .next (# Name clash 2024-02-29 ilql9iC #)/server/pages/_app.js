3835c39c835be1a3a5b6dc2eeca37f312082a8ec
Robin Zollner <code@bravegrumpy.com>
2024-03-02 12:03:25 -0500

    removing .next

--- .next (# Name clash 2024-02-29 ilql9iC #)/server/pages/_app.js
+++
@@ -1,4 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/pages/_app.js";
-const runtime = require("../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_4f7e5e._.js");
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/app.js [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
