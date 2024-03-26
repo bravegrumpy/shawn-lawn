9a5224bdfd935065b2e1e36b58c4f06f6945ed0b
Robin Zollner <code@bravegrumpy.com>
2024-03-26 08:28:28 +0000

    adding sign-in flow

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, SignOutButton, UserButton } from "@clerk/nextjs";
 import "@/app/globals.css";
 import { ubuntu  } from "@/fonts";
 import React from 'react';
@@ -12,7 +12,20 @@ export default function RootLayout({
     return(
         <ClerkProvider>
             <html lang="en-us">
-                <body className={`${ubuntu.className} bg-gradient-to-b from-emerald-200 dark:from-emerald-950 from-10% dark:from-10% via-lime-100 dark:via-lime-950 via-40% to-green-100 dark:to-green-950 to-80% bg-no-repeat`}>{children}</body>
+                <body className={`${ubuntu.className} bg-gradient-to-b from-emerald-200 dark:from-emerald-950 from-10% dark:from-10% via-lime-100 dark:via-lime-950 via-40% to-green-100 dark:to-green-950 to-80% bg-no-repeat`}>
+                    <header>
+                        <SignedOut>
+                            <SignInButton />
+                        </SignedOut>
+                        <SignedIn>
+                            <UserButton />
+                            <SignOutButton />
+                        </SignedIn>
+                    </header>
+                    <main>
+                        {children}
+                    </main>
+                </body>
             </html>
         </ClerkProvider>
     );
