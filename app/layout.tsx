8b5363f01d10d30e076e12e9db1efe7456c185d7
Robin Zollner <code@bravegrumpy.com>
2024-03-26 08:31:54 +0000

    removing signin flow

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,20 +12,7 @@ export default function RootLayout({
     return(
         <ClerkProvider>
             <html lang="en-us">
-                <body className={`${ubuntu.className} bg-gradient-to-b from-emerald-200 dark:from-emerald-950 from-10% dark:from-10% via-lime-100 dark:via-lime-950 via-40% to-green-100 dark:to-green-950 to-80% bg-no-repeat`}>
-                    <header>
-                        <SignedOut>
-                            <SignInButton />
-                        </SignedOut>
-                        <SignedIn>
-                            <UserButton />
-                            <SignOutButton />
-                        </SignedIn>
-                    </header>
-                    <main>
-                        {children}
-                    </main>
-                </body>
+                <body className={`${ubuntu.className} bg-gradient-to-b from-emerald-200 dark:from-emerald-950 from-10% dark:from-10% via-lime-100 dark:via-lime-950 via-40% to-green-100 dark:to-green-950 to-80% bg-no-repeat`}>{children}</body>
             </html>
         </ClerkProvider>
     );
