aae27a1347d3adb51049240092152bdd94516d93
Robin Zollner <code@bravegrumpy.com>
2024-03-26 07:21:30 +0000

    Adding clerk to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-
+import { ClerkProvider } from '@/clerk/nextjs';
 import "@/app/globals.css";
 import { ubuntu  } from "@/fonts";
 import React from 'react';
@@ -10,8 +10,10 @@ export default function RootLayout({
     children: React.ReactNode
 }){
     return(
-        <html lang="en-us">
-            <body className={`${ubuntu.className} bg-gradient-to-b from-emerald-200 dark:from-emerald-950 from-10% dark:from-10% via-lime-100 dark:via-lime-950 via-40% to-green-100 dark:to-green-950 to-80% bg-no-repeat`}>{children}</body>
-        </html>
+        <ClerkProvider>
+            <html lang="en-us">
+                <body className={`${ubuntu.className} bg-gradient-to-b from-emerald-200 dark:from-emerald-950 from-10% dark:from-10% via-lime-100 dark:via-lime-950 via-40% to-green-100 dark:to-green-950 to-80% bg-no-repeat`}>{children}</body>
+            </html>
+        </ClerkProvider>
     );
 }
