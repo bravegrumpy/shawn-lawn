a3c6ab7df29aa16e166a05eb594459caeee5b66b
Robin Zollner <code@bravegrumpy.com>
2024-03-26 11:52:27 -0400

    adding button

--- components/main.tsx
+++ components/main.tsx
@@ -2,7 +2,7 @@ import MainFlexContainer from "@/components/main-flex-container";
 import Header from '@/components/header';
 import ButtonLink from '@/components/button-link';
 import ImageLeftTextRight from '@/components/rows/image-left-text-right';
-import Form from "@/components/form-test";
+import Button from '@/components/button';
 
 export default function Main(){
     return (
@@ -10,11 +10,13 @@ export default function Main(){
                 {/* 
                     TODO: #33 Extract this element into a component.
                 */}
-                <MainFlexContainer>
+                <MainFlexContainer
+                    className="gap-2"
+                >
                     <Header
-                        className="-z-5 border-0 border-b2 border-x-2 border-solid border-neutral-500 rounded-none rounded-b-lg overflow-hidden -mb-1 mt-1" 
+                        className="z-5 border-0 border-b-2 border-x-2 border-solid border-neutral-500 rounded-none rounded-b-lg overflow-hidden -mb-1 mt-1"
                     />
-                    <div className="container self-center flex flex-col justify-start items-start gap-[50px] pt-[25px] m-auto bg-limeade-50 dark:bg-limeade-950">
+                    <div className="container self-center flex flex-col justify-start items-start gap-[50px] pt-[25px] m-auto bg-limeade-50 dark:bg-limeade-950 rounded-lg border-2 border-solid border-neutral-500">
                         <ImageLeftTextRight
                             src='https://img1.wsimg.com/isteam/ip/369b5ff8-e315-4151-9d33-e4bd5646e324/Screenshot_20220625-164121_Chrome~2.jpg/:/rs=w:800,h:800'
                             href="https://shawnlawn.com"
@@ -29,7 +31,7 @@ export default function Main(){
                             />
                         </div>
                     </div>
-                    <div className="container flex flex-col justify-start items-center dark:text-limeade-100 py-2 m-auto bg-emerald-50 dark:bg-emerald-950 gap-4">
+                    <div className="container flex flex-col justify-start items-center dark:text:white-1 py-2 m-auto bg-emerald-50 dark:bg-emerald-950 gap-4">
                         <p className="flex self-center text-brown-derby-700 dark:text-brown-derby-500">Email me at <br/> {/* 
                             TODO: #37 Extract this into a form
                             TODO: #36 Make form-subit button save info to cache, then has a popup with a sign-up form. Once signup is complete, message is sent, and confirmation is sent to user. 
@@ -44,10 +46,10 @@ export default function Main(){
                             </ol>
                         </div>
                     </div>
-                <div>
-                    <Form />
+                    <div className="container flex flex-col justify-start items-center dark:text-white-1 py-5 m-auto bg-white-1 dark:bg-black-1 gap-4 border-solid border-2 border-neutral-500 rounded-lg">
+                    <Button />
                 </div>
-                <div className="h-[1000px]"></div>
+                <div className="h-[300px]"></div>
             </MainFlexContainer>
         </>
     );
