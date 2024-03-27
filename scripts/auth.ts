7437df5d9a52c1494addc320cafa61833e51ab1d
Robin Zollner <code@bravegrumpy.com>
2024-03-26 22:31:11 -0400

    adding in github stuff

--- scripts/auth.ts
+++ scripts/auth.ts
@@ -1,3 +1,14 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
-export const { handlers, auth } = NextAuth({ providers: [ GitHub ] })
+
+export const { handlers, auth } = NextAuth(() => {
+    switch (req) {
+        case true:
+            console.log(req);
+            break;
+        case false:
+            console.log(req);
+            break;
+    }
+    return { providers: [ GitHub ]}
+});
