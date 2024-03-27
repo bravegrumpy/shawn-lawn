826268c9336d0a1291622d5fcfc1a1e30a82faac
Robin Zollner <code@bravegrumpy.com>
2024-03-26 22:54:00 -0400

    creating an auth.ts, a NextRoute

--- scripts/auth.ts
+++ scripts/auth.ts
@@ -1,14 +1,2 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
-
-export const { handlers, auth } = NextAuth(() => {
-    switch (req) {
-        case true:
-            console.log(req);
-            break;
-        case false:
-            console.log(req);
-            break;
-    }
-    return { providers: [ GitHub ]}
-});
